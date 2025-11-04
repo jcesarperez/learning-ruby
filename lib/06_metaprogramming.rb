d341dcb941defe2c2d2b60952135ae55302bb039
jcesarperez <julio.cesar.perez.arques@gmail.com>
2025-11-04 16:57:55 +0100

    Lesson 6 completed (#4)

--- lib/06_metaprogramming.rb
+++ lib/06_metaprogramming.rb
@@ -3,15 +3,18 @@
 # send - call methods dynamically
 class Calculator
   def add(a, b)
-    # TODO: Return a + b
+    # Return a + b
+    a + b
   end
 
   def subtract(a, b)
-    # TODO: Return a - b
+    # Return a - b
+    a - b
   end
 
   def multiply(a, b)
-    # TODO: Return a * b
+    # Return a * b
+    a * b
   end
 end
 
@@ -19,24 +22,25 @@ end
 class DynamicGreeter
   # Define methods for: hello, goodbye, welcome
   %w[hello goodbye welcome].each do |greeting|
-    # TODO: Use define_method to create a method for each greeting
+    # Use define_method to create a method for each greeting
     # The method should return "#{greeting.capitalize}!"
-    # Hint:
-    # define_method(greeting) do
-    #   "#{greeting.capitalize}!"
-    # end
+    define_method(greeting) do
+      "#{greeting.capitalize}!"
+    end
   end
 end
 
 # method_missing - intercept undefined method calls
 class MagicMethod
-  def method_missing(method_name, *args, &block)
-    # TODO: Return "You called: #{method_name}"
+  def method_missing(method_name, *_args, &)
+    # Return "You called: #{method_name}"
+    "You called: #{method_name}"
   end
 
-  def respond_to_missing?(method_name, include_private = false)
-    # TODO: Return true (always respond to any method)
+  def respond_to_missing?(_method_name, _include_private = false)
+    # Return true (always respond to any method)
     # This is good practice when defining method_missing
+    true
   end
 end
 
@@ -47,47 +51,45 @@ class DynamicAttributes
   end
 
   def method_missing(method_name, *args)
-    # TODO: Handle getter and setter methods
+    # Handle getter and setter methods
     # If method_name ends with '=', it's a setter: store in @attributes
     # Otherwise it's a getter: return from @attributes
-    # Hint:
-    # method_string = method_name.to_s
-    # if method_string.end_with?('=')
-    #   attribute_name = method_string.chomp('=').to_sym
-    #   @attributes[attribute_name] = args.first
-    # else
-    #   @attributes[method_name]
-    # end
+    method_string = method_name.to_s
+    if method_string.end_with?('=')
+      attribute_name = method_string.chomp('=').to_sym
+      @attributes[attribute_name] = args.first
+    else
+      @attributes[method_name]
+    end
   end
 
-  def respond_to_missing?(method_name, include_private = false)
+  def respond_to_missing?(_method_name, _include_private = false)
     true
   end
 end
 
 # class_eval / instance_eval
 def add_method_to_string
-  # TODO: Use class_eval to add a method to String class
+  # Use class_eval to add a method to String class
   # Add method 'loudly' that returns upcase version
   # Then return 'hello'.loudly
-  # Hint:
-  # String.class_eval do
-  #   define_method(:loudly) do
-  #     upcase
-  #   end
-  # end
+  String.class_eval do
+    define_method(:loudly) do
+      upcase
+    end
+  end
+  'hello'.loudly
 end
 
 class ModifiableObject
   def add_custom_method
-    # TODO: Use instance_eval to add a method to self
+    # Use instance_eval to add a method to self
     # Add method 'custom_greeting' that returns 'Custom Hello!'
-    # Hint:
-    # instance_eval do
-    #   def custom_greeting
-    #     'Custom Hello!'
-    #   end
-    # end
+    instance_eval do
+      def custom_greeting
+        'Custom Hello!'
+      end
+    end
   end
 end
 
@@ -101,17 +103,21 @@ class FlexibleUser
   attr_reader :created_fields
 
   def method_missing(method_name, *args)
-    # TODO: Similar to DynamicAttributes but also track created fields
+    # Similar to DynamicAttributes but also track created fields
     # When a setter is called, add the field name to @created_fields
     method_string = method_name.to_s
     if method_string.end_with?('=')
-      # TODO: setter logic
+      # setter logic
+      field_name = method_string.chomp('=').to_sym
+      @fields[field_name] = args.first
+      @created_fields << field_name unless @created_fields.include?(field_name)
     else
-      # TODO: getter logic
+      # getter logic
+      @fields[method_name]
     end
   end
 
-  def respond_to_missing?(method_name, include_private = false)
+  def respond_to_missing?(_method_name, _include_private = false)
     true
   end
 end
@@ -119,37 +125,44 @@ end
 # Open Classes (Monkey Patching)
 class Integer
   def double
-    # TODO: Return self * 2
+    # Return self * 2
+    self * 2
   end
 
   def triple
-    # TODO: Return self * 3
+    # Return self * 3
+    self * 3
   end
 end
 
 class String
   def shout
-    # TODO: Return "#{upcase}!"
+    # Return "#{upcase}!"
+    "#{upcase}!"
   end
 
   def whisper
-    # TODO: Return "#{self}..."
+    # Return "#{downcase}..."
+    "#{downcase}..."
   end
 end
 
 # DSL Example
 class Configuration
-  def initialize(&block)
+  def initialize(&)
     @settings = {}
-    # TODO: Execute the block in the context of this instance
+    # Execute the block in the context of this instance
     # Hint: instance_eval(&block) if block_given?
+    instance_eval(&) if block_given?
   end
 
   def set(key, value)
-    # TODO: Store key-value pair in @settings
+    # Store key-value pair in @settings
+    @settings[key] = value
   end
 
   def get(key)
-    # TODO: Retrieve value from @settings
+    # Retrieve value from @settings
+    @settings[key]
   end
 end
